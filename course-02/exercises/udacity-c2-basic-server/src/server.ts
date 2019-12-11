3309ce613f73a315fef90440f2e48319502c0b88
nikos <nikoskordis@gmail.com>
2019-12-11 15:12:16 +0100

    Add endpoints
    Add an endpoint to GET a list of cars
    Add an endpoint to get a specific car
    Add an endpoint to post a new car to our list

--- course-02/exercises/udacity-c2-basic-server/src/server.ts
+++ course-02/exercises/udacity-c2-basic-server/src/server.ts
@@ -68,16 +68,74 @@ import { Car, cars as cars_list } from './cars';
                 .send(`Welcome to the Cloud, ${name}!`);
   } );
 
-  // @TODO Add an endpoint to GET a list of cars
+  // GET a list of cars
   // it should be filterable by make with a query paramater
+  app.get( "/cars/", ( req: Request, res: Response ) => {
+    let { make } = req.query;
 
-  // @TODO Add an endpoint to get a specific car
+    if ( !make ) {
+      return res.status(200)
+                .json(cars_list);
+    }else{
+
+    return res.status(200)
+              .json(cars_list.filter(function(car){   
+                  return car.make == make? car: null;                               
+                  })
+                );
+          }
+  } );
+
+  // Get a specific car
   // it should require id
   // it should fail gracefully if no matching car is found
+  app.get( "/cars/:id", ( req: Request, res: Response ) => {
+    let { id } = req.params;
+     if ( !id ) {
+      return res.status(400)
+                .send(`id is required`);
+    }
 
-  /// @TODO Add an endpoint to post a new car to our list
+    if(Number(id) == NaN){
+      return res.status(400)
+                .send('Bad request');
+    }
+
+    if(id > cars_list.length - 1){
+      return res.status(404)
+                .send('item does not exist.');
+    }
+
+    return res.status(200)
+              .json(cars_list.find(car => car.id == id));                                         
+  } );
+
+  
+
+
+  
+
+  /// Post a new car to our list
   // it should require id, type, model, and cost
+  app.post( "/cars", 
+  async ( req: Request, res: Response ) => {
 
+    const { make, type, model, cost, id } = req.body;
+
+    if ( !make  || !type || !model || !cost || !id) {
+      return res.status(400)
+                .send(`name, type, model, cost, id are required`);
+    }
+
+    const newCar:Car = {
+      make: make, type: type, model: model, cost: cost, id: id
+    }
+
+    cars_list.push(newCar);
+
+    return res.status(201)
+              .send(newCar);
+} );
   // Start the Server
   app.listen( port, () => {
       console.log( `server running http://localhost:${ port }` );
