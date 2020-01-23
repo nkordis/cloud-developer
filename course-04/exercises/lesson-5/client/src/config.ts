ca912422b0fc411939d1c1ca4d508dba6c229de4
nikos <nikoskordis@gmail.com>
2020-01-23 15:41:05 +0100

    Configure the web application

--- course-04/exercises/lesson-5/client/src/config.ts
+++ course-04/exercises/lesson-5/client/src/config.ts
@@ -1,7 +1,7 @@
 export const apiEndpoint = '...'
 
 export const authConfig = {
-  domain: '...',
-  clientId: '...',
+  domain: 'dev-u23y6q-y.eu.auth0.com',
+  clientId: 'qC3hYkHe1uaVkwnPQJueetlQTC8YLJJF',
   callbackUrl: 'http://localhost:3000/callback'
 }
