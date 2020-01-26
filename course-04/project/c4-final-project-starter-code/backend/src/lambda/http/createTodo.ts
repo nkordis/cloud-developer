c642e1bae3a4089b1e1fac23a7b905834ac75217
nikos <nikoskordis@gmail.com>
2020-01-26 17:52:15 +0100

    Add  lambda to create a new todo item

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
@@ -4,9 +4,37 @@ import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } f
 
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 
+import * as AWS  from 'aws-sdk'
+import * as uuid from 'uuid'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+const groupsTable = process.env.TODOS_TABLE
+
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  console.log('Processing event: ', event)
+  const itemId = uuid.v4()
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
 
-  // TODO: Implement creating a new TODO item
-  return undefined
+  // TODO: Implement creating a new TODO item 
+  const newItem = {
+    createdAt : new Date().toISOString(),
+    todoId: itemId,
+    ...newTodo,
+    done: false
+  }
+
+  await docClient.put({
+    TableName: groupsTable,
+    Item: newItem
+  }).promise()
+
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      newItem
+    })
+  }
 }
