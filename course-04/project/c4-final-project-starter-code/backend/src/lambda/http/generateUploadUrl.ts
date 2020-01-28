b5db291c9380e6c61c52d2714f6f28efa060c70b
nikos <nikoskordis@gmail.com>
2020-01-28 14:12:06 +0100

    Return a pre-signed url to upload an attachment file for a TODO item

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/generateUploadUrl.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/generateUploadUrl.ts
@@ -1,10 +1,32 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS  from 'aws-sdk'
+
+const s3 = new AWS.S3({
+  signatureVersion: 'v4'
+})
+
+const bucketName = process.env.TODOS_S3_BUCKET
+const urlExpiration = process.env.SIGNED_URL_EXPIRATION
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
 
   // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+  const url = getUploadUrl(todoId)
+  return {
+    statusCode: 201,
+    body: JSON.stringify({
+      uploadUrl: url
+    })
+  }
+}
+
+function getUploadUrl(todoId: string) {
+  return s3.getSignedUrl('putObject', {
+    Bucket: bucketName,
+    Key: todoId,
+    Expires: parseInt(urlExpiration)
+  })
 }
