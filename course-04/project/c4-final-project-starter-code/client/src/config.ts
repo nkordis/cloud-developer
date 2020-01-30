610d6260fd0f0231295eaf1a6d08774f83385b2f
nikos <nikoskordis@gmail.com>
2020-01-30 16:18:00 +0100

    Configure the react client

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = '2kn0l67cdc'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-u23y6q-y.eu.auth0.com',            // Auth0 domain
+  clientId: 'LwEoS9OjUwDrPHrCfDvie2Z4XYtUqICf',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
