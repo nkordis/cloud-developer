e34ee7461c0d0a3f655934993ca4632378834c66
nikos <nikoskordis@gmail.com>
2020-02-03 15:59:35 +0100

    Delete a todo item with a composite key

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/deleteTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/deleteTodo.ts
@@ -3,19 +3,57 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
 import * as AWS  from 'aws-sdk'
+import { parseUserId } from '../../auth/utils'
+import { createLogger } from '../../utils/logger'
 
+const logger = createLogger('createTodo')
 const docClient = new AWS.DynamoDB.DocumentClient()
 const todosTable = process.env.TODOS_TABLE
+const userIdIndex = process.env.INDEX_NAME
 
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const todoId = event.pathParameters.todoId
+  logger.info('Processing event: ', event)
 
+  const authorization = event.headers.Authorization 
+  const split = authorization.split(' ')
+  const jwtToken = split[1]
+
+  const result = await docClient.query({
+    TableName : todosTable,
+    IndexName : userIdIndex,
+    KeyConditionExpression: 'userId = :userId',
+    ExpressionAttributeValues: {
+        ':userId': parseUserId(jwtToken)
+    }
+}).promise()
+
+  const todoId = event.pathParameters.todoId
+  let itemToSelect = -1;
+  if (result.Count !== 0) {
+  for(let i = 0; i < result.Items.length; i++){
+    if(result.Items[i]["todoId"] === todoId){
+      itemToSelect = i;
+    }
+  }
+
+  if(itemToSelect === -1){
+    
+      return {
+        statusCode: 404,
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: ''
+      }
+    
+  }
   // TODO: Remove a TODO item by id
   const params = {
     TableName:todosTable,
     Key:{
-        "todoId": todoId
+        "todoId": todoId,
+        "createdAt": result.Items[itemToSelect]["createdAt"]
     }
   }
   await docClient.delete(params).promise()
@@ -27,4 +65,15 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
     },
     body: ''
   }
+
+}
+
+return {
+  statusCode: 404,
+  headers: {
+    'Access-Control-Allow-Origin': '*'
+  },
+  body: ''
+}
+
 }
