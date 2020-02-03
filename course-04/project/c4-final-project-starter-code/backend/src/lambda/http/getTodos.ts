a79b7b1992600fa820fa07822bca1e54cf98a827
nikos <nikoskordis@gmail.com>
2020-02-03 15:56:41 +0100

    Get todos by user id

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
@@ -3,15 +3,50 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
 import * as AWS  from 'aws-sdk'
+import { parseUserId } from '../../auth/utils'
+import { createLogger } from '../../utils/logger'
 
+const logger = createLogger('createTodo')
 const docClient = new AWS.DynamoDB.DocumentClient()
 
 const todosTable = process.env.TODOS_TABLE
+const userIdIndex = process.env.INDEX_NAME
+
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // TODO: Get all TODO items for a current user
-  console.log('Processing event: ', event)
+  logger.info('Processing event: ', event)
 
+  const authorization = event.headers.Authorization 
+  const split = authorization.split(' ')
+  const jwtToken = split[1]
+
+  const result = await docClient.query({
+    TableName : todosTable,
+    IndexName : userIdIndex,
+    KeyConditionExpression: 'userId = :userId',
+    ExpressionAttributeValues: {
+        ':userId': parseUserId(jwtToken)
+    }
+}).promise()
+
+if (result.Count !== 0) {
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify(result.Items)
+  }
+}
+
+return {
+  statusCode: 404,
+  headers: {
+    'Access-Control-Allow-Origin': '*'
+  },
+  body: ''
+}
+/*
   const result = await docClient.scan({
     TableName: todosTable
   }).promise()
@@ -27,5 +62,5 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
       items
     })
   }
-
+*/
 }
